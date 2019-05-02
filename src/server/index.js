9257ccd070a43edc4b6f0f55ade8c085091cd776
Yonas Alem <yonasalem056@gmail.com>
2019-05-02 14:42:48 +0300

    file uploads

--- src/server/index.js
+++ src/server/index.js
@@ -1,32 +1,37 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = 4000;
-const cors = require('cors');
-const mongoose = require('mongoose');
-const morgan  = require('mor')
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
 
-const config = require('./DB');
+const config = require("./DB");
 
-let userRouter = require('./routes/user.route');
-let productRouter = require('./routes/products');
-let orderRouter = require('./routes/orders');
+let userRouter = require("./routes/user.route");
+let productRouter = require("./routes/products");
+let orderRouter = require("./routes/orders");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-    () => {console.log('Database is connected') },
-    err => { console.log('Can not connect to the database'+ err)}
+    () => {
+        console.log("Database is connected");
+    },
+    err => {
+        console.log("Can not connect to the database" + err);
+    }
 );
 
+app.use(morgan("dev"));
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/uploads',express.static('uploads'));
 
+app.use("/user", userRouter);
+app.use("/products", productRouter);
+app.use("/orders", orderRouter);
 
-app.use('/user',userRouter);
-app.use('/products',productRouter);
-app.use('/orders',orderRouter);
-
-app.listen(PORT, function(){
-    console.log('Server is running on Port:',PORT);
+app.listen(PORT, function() {
+    console.log("Server is running on Port:", PORT);
 });
--- src/server/routes/products.js
+++ src/server/routes/products.js
@@ -1,140 +1,166 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const multer = require("multer");
 
+//filter the kinds of images to be stored.
+const imageFilter = (req, file, cb) => {
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpeg") {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+let upload = multer({
+    storage: storage,
+    limits: 1024 * 1024 * 10,
+    fileFilter: imageFilter
+});
 let router = express.Router();
 let Product = require("../models/product.model");
 
 //get all the products
 router.get("/", (req, res, next) => {
-  Product.find()
-    .select("name price _id")
-    .exec()
-    .then(results => {
-      const response = {
-        count: results.length,
-        products: results.map(result => {
-          return {
-            name: result.name,
-            price: result.price,
-            _id: result._id,
-            request: {
-              type: "GET",
-              url: "http://localhost:4000/products/" + result._id
-            }
-          };
+    Product.find()
+        .select("name price _id productImage")
+        .exec()
+        .then(results => {
+            const response = {
+                count: results.length,
+                products: results.map(result => {
+                    return {
+                        name: result.name,
+                        price: result.price,
+                        _id: result._id,
+                        productImage: result.productImage,
+                        request: {
+                            type: "GET",
+                            url: "http://localhost:4000/products/" + result._id
+                        }
+                    };
+                })
+            };
+            res.status(200).json(response);
         })
-      };
-      res.status(200).json(response);
-    })
-    .catch(err => {
-      res.status(404).json({
-        message: `unable to find any data`,
-        error: err
-      });
-    });
+        .catch(err => {
+            res.status(404).json({
+                message: `unable to find any data`,
+                error: err
+            });
+        });
 });
 
 //add a product
-router.post("/", (req, res, next) => {
-  //set up the inputs for the database
-  let product = new Product({
-    _id: mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price
-  });
-
-  product
-    .save()
-    .then(result => {
-      res.status(201).json({
-        message: `Product Created Successfully.`,
-        createdProduct: {
-          name: result.name,
-          price: result.price,
-          _id: result._id,
-          request: {
-            type: "GET",
-            url: "http://localhost:4000/products/" + result._id
-          }
-        }
-      });
-    })
-    .catch(err => {
-      console.log(`unable to save ${err}`);
-      res.status(404).json({
-        Error: err
-      });
+router.post("/", upload.single("productImage"), (req, res, next) => {
+    //set up the inputs for the database
+    let product = new Product({
+        _id: mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price,
+        productImage: req.file.path
     });
+
+    product
+        .save()
+        .then(result => {
+            res.status(201).json({
+                message: `Product Created Successfully.`,
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:4000/products/" + result._id
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(`unable to save ${err}`);
+            res.status(404).json({
+                Error: err
+            });
+        });
 });
 
 //find the individual products
 router.get("/:productId", (req, res, next) => {
-  const id = req.params.productId;
+    const id = req.params.productId;
 
-  Product.findById(id)
-    .select("name price _id")
-    .exec()
-    .then(result => {
-      res.status(200).json({
-        message: `Product with Id ${id}`,
-        response: {
-          result: result,
-          request: {
-            type: 'GET',
-            description: 'Get all Products',
-            url: "http://localhost:4000/products/"
-          }
-        }
-      });
-    })
-    .catch(err => {
-      res.status(404).json({
-        message: `unable to find data with this ${id}`,
-        Error: err
-      });
-    });
+    Product.findById(id)
+        .select("name price _id")
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: `Product with Id ${id}`,
+                response: {
+                    result: result,
+                    request: {
+                        type: "GET",
+                        description: "Get all Products",
+                        url: "http://localhost:4000/products/"
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            res.status(404).json({
+                message: `unable to find data with this ${id}`,
+                Error: err
+            });
+        });
 });
 
 //update the individual products
 router.patch("/:productId", (req, res, next) => {
-  let id = req.params.productId;
-  Product.findOneAndUpdate(id, {
-    $set: { name: req.body.newName, price: req.body.newPrice }
-  })
-    .exec()
-    .then(result => {
-      res.status(201).json({
-        message: `Product with id ${id} successfully updated.`,
-        result: result,
-        request: {
-          type: 'GET',
-          url: "http://localhost:4000/products/"+id
-        }
-      });
+    let id = req.params.productId;
+    Product.findOneAndUpdate(id, {
+        $set: { name: req.body.newName, price: req.body.newPrice }
     })
-    .catch(err => {
-      res.status(500).json({
-        message: `unable to update data`,
-        error: err
-      });
-    });
+        .exec()
+        .then(result => {
+            res.status(201).json({
+                message: `Product with id ${id} successfully updated.`,
+                result: result,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:4000/products/" + id
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: `unable to update data`,
+                error: err
+            });
+        });
 });
 
 //update the individual products
 router.delete("/:productId", (req, res, next) => {
-  let id = req.params.productId;
-  Product.findByIdAndRemove(id)
-    .exec()
-    .then(() => {
-      res.status(200).json({
-        message: `product with id ${id} successfully deleted.`
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: `unable to delete.`
-      });
-    });
+    let id = req.params.productId;
+    Product.findByIdAndRemove(id)
+        .exec()
+        .then(() => {
+            res.status(200).json({
+                message: `product with id ${id} successfully deleted.`
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: `unable to delete.`
+            });
+        });
 });
 
 module.exports = router;
