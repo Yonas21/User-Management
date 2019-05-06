c802b91b9cfcc3a639c7f934d318fb6d9fbeb6f3
Yonas Alem <yonasalem056@gmail.com>
2019-05-06 09:02:17 +0300

    logout

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../user.service";
+import { Router } from "@angular/router";
+import { NgFlashMessageService } from "ng-flash-messages";
 
 @Component({
-  selector: 'app-homepage',
-  templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+    selector: "app-homepage",
+    templateUrl: "./homepage.component.html",
+    styleUrls: ["./homepage.component.css"]
 })
 export class HomepageComponent implements OnInit {
+    constructor(
+        private userService: UserService,
+        private router: Router,
+        private flashMessage: NgFlashMessageService
+    ) {}
 
-  constructor() { }
+    ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+    onLogoutClick() {
+        this.userService.logout();
+        this.flashMessage.showFlashMessage({
+          messages: ['You Logged out Successfully.'],
+          dismissible: true,
+          timeout: 5000,
+          type: 'info'
+        })
+    }
 }
--- src/app/tokenPayload.model.ts
+++
@@ -1,4 +0,0 @@
-export interface TokenPayloadModel {
-    id: string;
-    email: string;
-}
