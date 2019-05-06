9bdfbc64af5c114964ce81455669da177c9356e9
Yonas Alem <yonasalem056@gmail.com>
2019-05-06 10:32:11 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes} from '@angular/router';
 
 // angular components
-import { SignupComponent} from './signup/signup.component';
-import {LoginComponent} from './login/login.component';
-import {HomepageComponent} from './homepage/homepage.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ContactComponent } from "./contact/contact.component";
+import { ProductCategoryComponent } from "./product-category/product-category.component";
+import { ProductDetailComponent } from "./product-detail/product-detail.component";
+import { WishlistComponent } from "./wishlist/wishlist.component";
 
 const routes: Routes = [
     {
@@ -24,6 +30,30 @@ const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+      path:'cart',
+      component: CartComponent
+    },
+    {
+      path: 'checkout',
+      component: CheckoutComponent
+    },
+    {
+      path: 'contact',
+      component: ContactComponent
+    },
+    {
+      path: 'product-category',
+      component: ProductCategoryComponent
+    },
+    {
+      path: 'product-detail',
+      component:ProductDetailComponent
+    },
+    {
+      path:'wishlist',
+      component:  WishlistComponent
     }
 ];
 
